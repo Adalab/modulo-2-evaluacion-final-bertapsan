1abb01996c1c994b618354a2e002811798a904c1
bertapsan <bertapsan@gmail.com>
2021-02-09 14:14:32 +0100

    Add search filter and prevent default

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 'use strict';
 
-const showsElement = document.querySelector('.js-shows');
+const showsElement = document.querySelector('.js-shows');           // mostrar los resultados de la búsqueda
+const searchElement = document.querySelector('.js-form__input');   // recoger el texto que teclea la usuaria
+const formButtonElement = document.querySelector('.js-form__button'); // llamarla en el listner de la búsqueda
+const formElement = document.querySelector('.js-form');
 
 // api
 
-fetch('http://api.tvmaze.com/search/shows?q=girls')
+function callToApi() {   // Genero una función para llamar a la api, facilitar el search
+fetch('http://api.tvmaze.com/search/shows?q=' + searchElement.value) // a la url de la api le he añadido un value para generar la búsqueda
   .then((response) => response.json())
   .then((data) => {
     paintResultSearch (data)
   });
-
-
+}
 // paint
 
-function paintResultSearch (resultSearch) {
+function paintResultSearch (resultSearch) {   
   let htmlCode = '';
     for (const film of resultSearch) {
       htmlCode += '<li>';
@@ -21,7 +24,7 @@ function paintResultSearch (resultSearch) {
       if (film.show.image === null) {                 // condicional para cuando no tengamos imagen asociada, si es null me muestras imagen dummy
         htmlCode += '<p> <img src="https://via.placeholder.com/210x295/ffffff/666666/?"/>';
       } else {
-        htmlCode += `<p><img src="${film.show.image.medium}"/></p>`;  //la imagen no está en la raiz y además API ofrece varios formatos, selecciono medium uniformizar
+        htmlCode += `<p><img src="${film.show.image.medium}"/></p>`;  //la imagen no está en la raiz y además API ofrece varios formatos, selecciono medium para uniformizar
       }
       htmlCode += '</li>';
     }
@@ -31,5 +34,19 @@ function paintResultSearch (resultSearch) {
 
 // local storage
 //search
+
+function handleSearch() {
+  /* console.log('hola', searchElement.value); */
+  callToApi();
+}
+formButtonElement.addEventListener('click', handleSearch);  // el listner lo pongo sobre el botón de búsqueda
+
+
+// submit form - prevent
+function handleForm(ev) { // ojo el prevent se hace sobre el form no sobre el botón!
+  ev.preventDefault();
+}
+formElement.addEventListener('submit', handleForm);
+
 // filter
 // start app
